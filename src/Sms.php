0af6b21e0956be983dbccbe20c10b26a7ce759fc
firmin <firminapp@gmail.com>
2020-05-09 13:36:29 +0100

    initial commit

---
+++ src/Sms.php
@@ -0,0 +1,20 @@
+<?php 
+namespace Smsphp ;
+
+class Sms
+{
+    /**
+     * methode to send new sms
+     * @param $api_key your api_key if you don't have it yet get one from here  <a href="https://sms.coursesroom.com">get your api_key</a>
+     * @param $to phone number to send sms to
+     * @param $from your company name or the title you wicth it appear for this sms
+     * @param $body content of your sms
+     */
+    public static function send($api_key,$to,$from,$body)
+    {
+
+
+        return 'Hello World, Composer!';
+    }
+
+}
